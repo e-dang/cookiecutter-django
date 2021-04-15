193de08303eb8276b431933bca74921c0c93e7a7
Eric Dang <edang830@gmail.com>
2021-04-15 11:37:44 -0700

    Add JsonFactoryMixin

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tests/factories.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tests/factories.py
@@ -1,11 +1,41 @@
-from typing import Any, Sequence
+from functools import partial
+from typing import Any, Dict, Sequence
 
 from django.contrib.auth import get_user_model
 from factory import Faker, post_generation
+from factory.base import StubObject
 from factory.django import DjangoModelFactory
 
 
-class UserFactory(DjangoModelFactory):
+def generate_dict_factory(factory: Factory):
+    """https://github.com/FactoryBoy/factory_boy/issues/68"""
+
+    def convert_dict_from_stub(stub: StubObject) -> Dict[str, Any]:
+        stub_dict = stub.__dict__
+        for key, value in stub_dict.items():
+            if isinstance(value, StubObject):
+                stub_dict[key] = convert_dict_from_stub(value)
+        return stub_dict
+
+    def dict_factory(factory, **kwargs):
+        stub = factory.stub(**kwargs)
+        stub_dict = convert_dict_from_stub(stub)
+        return stub_dict
+
+    return partial(dict_factory, factory)
+
+
+class JsonFactoryMixin:
+    @classmethod
+    def json(cls, **kwargs):
+        return generate_dict_factory(cls)(**kwargs)
+
+    @classmethod
+    def json_batch(cls, num, **kwargs):
+        factory = generate_dict_factory(cls)
+        return [factory(**kwargs) for _ in range(num)]
+
+class UserFactory(DjangoModelFactory, JsonFactoryMixin):
 
     {% if cookiecutter.user.username_field == "username" -%}
     username = Faker("user_name")
