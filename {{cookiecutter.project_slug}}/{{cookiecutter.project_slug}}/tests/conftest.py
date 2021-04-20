501f9b9e969b60ad19401e97da2fbf1bb255941e
Eric Dang <edang830@gmail.com>
2021-04-20 16:19:30 -0700

    Add marks to tests based on directory they are in

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tests/conftest.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/tests/conftest.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pytest
 from pytest_factoryboy import register as factory_register
 from pytest_factoryboy.fixture import get_caller_module, get_model_name, make_fixture
@@ -13,14 +15,28 @@ def register(factory_class, _name=None, **kwargs):
     factory_register(factory_class, _name=_name, **kwargs)
     module = get_caller_module()
     model_name = get_model_name(factory_class) if _name is None else _name
-    name = f'{model_name}_json'
+    name = f"{model_name}_json"
 
     make_fixture(name, module, json_fixture, factory_class=factory_class)
+
+
+register(UserFactory)
+
+
+def pytest_collection_modifyitems(config, items):
+    """Give tests a specific mark based on the test type directory they are in"""
+
+    rootdir = Path(config.rootdir)
+    for item in items:
+        rel_path = Path(item.fspath).relative_to(rootdir)
+        if "unit" in str(rel_path):
+            item.add_marker("unit")
+        elif "integration" in str(rel_path):
+            item.add_marker("integration")
+        elif "functional" in str(rel_path):
+            item.add_marker("functional")
 
 
 @pytest.fixture(autouse=True)
 def media_storage(settings, tmpdir):
     settings.MEDIA_ROOT = tmpdir.strpath
-
-
-register(UserFactory)
